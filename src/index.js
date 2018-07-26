2ffa2bd7ad50723a8460860a04a0f829582aedc3
sarcoma <sarcoma@live.co.uk>
2018-07-26 13:26:06 +0100

    Add fontawesome pro

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom';
 import Root from './app/components/root';
 import configureStore from './configureStore';
 
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faBell, faCoffee} from '@fortawesome/pro-solid-svg-icons';
+
 import './sass/main.scss';
+
+library.add(faBell, faCoffee);
 
 const store = configureStore();
 
